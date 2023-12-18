4c1e204d53144fee27219ae0e29785c3b890a03b
songheebaek <grape030103@naver.com>
2023-12-18 23:07:22 +0900

    기말고사 최종

--- board/urls.py
+++ board/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name='list'),
+    path('', views.index,name='login'),
+    path('daily_connect/', views.afterlogin, name='daily_connect'),
     path('<int:board_id>/', views.detail, name='detail'),
     path('create/', views.create.as_view(), name='create'),
 ]
--- board/views.py
+++ board/views.py
@@ -5,24 +5,35 @@ from django.http import HttpResponse
 from .models import Board
 from django.views import generic
 from django.urls import reverse_lazy
-
-# def index(request):
-    # return HttpResponse("Hello")
+from django.contrib.auth import authenticate, login
 
 def index(request):
-   board_list = Board.objects.order_by('create_date')
-   context = {'board_list': board_list}
-   return render(request, 'board/board_list.html',context)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return afterlogin(request)
+        else:
+            return render(request, 'board/board_login.html', {'error_message': '아이디 혹은 비밀번호가 잘못되었습니다.'})
+    return render(request, 'board/board_login.html')
 
 def detail(request, board_id):
     board = Board.objects.get(id=board_id)
     context = {'board' : board}
     return render(request, 'board/board_detail.html', context)
 
+def afterlogin(request):
+   board_list = Board.objects.order_by('create_date')
+   context = {'board_list': board_list}
+   return render(request, 'board/board_list.html',context)
+
 class create(generic.CreateView):
     model = Board
     fields = ['subject', 'content', 'create_date']
-    success_url = reverse_lazy('board_list')
+    success_url = reverse_lazy(afterlogin)
     template_name_suffix = '_create'
 
     
